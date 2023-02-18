6991a3c852f733a4a80f4b60ede8bc179bf4bc9f
Felipe Pêpe <felipepepe21@gmail.com>
2023-02-18 12:47:58 -0300

    Adjusted navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -7,51 +7,34 @@ import { HiMenu } from "react-icons/hi";
 import styles from "./navbar.module.css";
 import utilStyles from "../../styles/utils.module.css";
 
-export interface NavbarProps {
-  isHome: boolean;
-}
-
-const Navbar: React.FC<NavbarProps> = ({ isHome }) => {
+const Navbar: React.FC = () => {
   const [hamburgerMenuActive, setHamburgerMenuActive] = useState(false);
 
   return (
     <div className={styles.navbar}>
-      {isHome ? (
-        <div className={styles.mainHome}>
-          <button
-            className={styles.hamburgerButton}
-            onClick={() => {
-              setHamburgerMenuActive(!hamburgerMenuActive);
-            }}
-          >
-            <HiMenu className={styles.hamburgerIcon} />
-          </button>
-        </div>
-      ) : (
-        <div className={styles.main}>
-          <Link href={"/"}>
-            <div className={styles.title}>
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={40}
-                width={40}
-                alt={"Felipe Pêpe"}
-              />
-              <h1 className={styles.titleHeading}>felipe pêpe</h1>
-            </div>
-          </Link>
-          <button
-            className={styles.hamburgerButton}
-            onClick={() => {
-              setHamburgerMenuActive(!hamburgerMenuActive);
-            }}
-          >
-            <HiMenu className={styles.hamburgerIcon} />
-          </button>
-        </div>
-      )}
+      <div className={styles.main}>
+        <Link href={"/"}>
+          <div className={styles.title}>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={40}
+              width={40}
+              alt={"Felipe Pêpe"}
+            />
+            <h1 className={styles.titleHeading}>felipe pêpe</h1>
+          </div>
+        </Link>
+        <button
+          className={styles.hamburgerButton}
+          onClick={() => {
+            setHamburgerMenuActive(!hamburgerMenuActive);
+          }}
+        >
+          <HiMenu className={styles.hamburgerIcon} />
+        </button>
+      </div>
 
       {/* Menu mobile */}
       {hamburgerMenuActive && (
