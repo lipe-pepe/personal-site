a8a1a1fd6a72915ae289d16eae5f1efc40e6449a
Felipe Pêpe <felipepepe21@gmail.com>
2023-02-23 00:12:47 -0300

    Changed date locale

--- components/date.tsx
+++ components/date.tsx
@@ -1,6 +1,12 @@
 import { parseISO, format } from "date-fns";
+import { pt } from "date-fns/locale";
 
 export default function Date({ dateString }: { dateString: string }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+
+  return (
+    <time dateTime={dateString}>
+      {format(date, "dd MMM, yyyy", { locale: pt })}
+    </time>
+  );
 }
